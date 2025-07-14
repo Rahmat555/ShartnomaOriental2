8e993494a399e983d1b5e73d3769c6ec57f7adeb
Rahmat555 <86076228+Rahmat555@users.noreply.github.com>
2025-07-14 15:03:38 +0500

    Update server.js

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.post('/api/generate-pdf', async (req, res) => {
     page2.drawText(jshshir, { x: 375, y: 180, size: 10, font });
     page2.drawText(phone, { x: 345, y: 170, size: 9, font });
     page2.drawText(`Sana: ${birthdate}`, { x: 321, y: 141, size: 10, font });
-    page2.drawText(operator, { x: 321, y: 111, size: 10, font });
+    page2.drawText(operator, { x: 100, y: 111, size: 10, font });
 
     page1.drawText(`${Number(contractAmount).toLocaleString()} so‘m`, {
       x: 400,
